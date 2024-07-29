574c8b4b1f06b287e04b37f60f12a1eab86807ec
janagithub007 <janardhanan.s007@gmail.com>
2024-07-29 16:10:03 +0530

    cm changes

---
+++ README.md
@@ -0,0 +1,19 @@
+# Day 5 Task 
+##  Iterations using different types of for loops
+### JSON RESUME DATA
+ * Here I created myresume data in the *JSON* format in the seperate file which is in the folder [iterations](./iterations)
+ * You can check my code in the file [script.js](./iterations/script.js)
+ ### FOR LOOP ITERATION
+ * I iterate the above JSON array via for loop . 
+ * The code it is in the file called [forloop.js](./iterations/forloop.js)
+  ### FOR IN ITERATION
+ * I iterate the above JSON array via for IN loop . 
+ * The code it is in the file called [forin.js](./iterations/forin.js)
+  ### FOR  OF LOOP ITERATION
+ * I iterate the above JSON array via for  of loop . 
+ * The code it is in the file called [forof.js](./iterations/forof.js)
+  ### FOREACH LOOP ITERATION
+ * I iterate the above JSON array via foreach loop . 
+ * The code it is in the file called [foreach.js](./iterations/foreach.js)
+
+  
---
+++ iterations/forof.js
@@ -0,0 +1,69 @@
+const myresume =[{
+    "basics": {
+      "name": "Janardhanan s",
+      "label": "Programmer",
+      "email": "janardhanan.s007@gmail.com",
+      "phone": "(912) 555-4321",
+      
+      "summary": "A summary of Janardhanan...",
+      "location": {
+        "address": "01 ",
+        "postalCode": "631531",
+        "city": "Kanchipuram",
+        "countryCode": "IN",
+        "region": "Hindhu"
+      },
+      "profiles": [
+        {
+          "network": "Github",
+          "username": "Janagithub007",
+          "url": "https://github.com/janagithub007"
+         
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "RAJALAKSHMI INSTITUTE OF TECHNOLOGY",
+        
+        "area": "E.C.E",
+        "studyType": "Bachelor",
+        "startDate": "2020-09-01",
+        "endDate": "2024-06-01",
+        "score": "4.0",
+       
+      }
+    ],
+    "skills": [
+     {
+        "name": "Web Development",
+        "level": "Master",
+        "keywords": ["HTML", "CSS", "JavaScript"]
+      }
+    ],
+    "languages": [
+      {
+        "language": "English",
+       
+      }
+    ],
+    "interests": [
+      {
+        "name": "Wildlife",
+        "keywords": ["Ferrets", "Unicorns"]
+      }
+    ],
+   
+    
+  }]
+//   for of iteration 
+ for(let resume of myresume){
+    console.log( resume );
+ }
+  
+  
+  
+  
+  
+  
